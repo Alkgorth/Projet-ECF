c71a479f8f6db002a2557e4727c93de6a22f3251
Alkgorth <ap.percheron@gmail.com>
2024-10-30 17:40:17 +0100

    modif header

--- templates/header.php
+++ templates/header.php
@@ -13,19 +13,22 @@ use App\Tools\Navigation;
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse ms-auto" id="navbarNav">
+      <div class="collapse navbar-collapse ms-auto justify-content-end" id="navbarNav">
         <ul class="navbar-nav">
           <li class="nav-item">
             <a class="nav-link" href="index.php?controller=pages&action=panier"><i class="bi bi-cart-plus fs-2"></i></a>
           </li>
-          <li class="nav-item">
+          <li class="nav-item dropdown">
             <?php if (isset($_SESSION['user'])) { ?>
-              <a class="nav-link" href="index.php?controller=connexions&action=deconnexion"><i class="bi bi-person-circle fs-2">Déconnexion</i></a>
+              <a class="nav-link dropdown-toggle" href="index.php?controller=connexions&action=deconnexion" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="bi bi-person-circle fs-2"></i></a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a class="dropdown-item" href="index.php?controller=pages&action=espacePersonnel">Profil</a></li>
+                <li><a class="dropdown-item" href="index.php?controller=pages&action=panier">Panier</a></li>
+                <li><a class="dropdown-item" href="index.php?controller=connexions&action=deconnexion">Déconnexion</a></li>
+              </ul>
             <?php } else { ?>
-            <!-- <a class="nav-link" href="index.php?controller=connexions&action=connexion"><i class="bi bi-person-square fs-2"></i></a> -->
             <a href="/index.php?controller=connexions&action=connexion" class="btn btn-outline-primary me-2 <?= Navigation::addActiveClass('connexions', 'connexion') ?>">Connexion</a>
             <a href="/index.php?controller=pages&action=creationCompte" class="btn btn-outline-primary me-2 <?= Navigation::addActiveClass('pages', 'creationCompte') ?>">Inscription</a>
-            
             <?php } ?>
           </li>
           <li class="nav-item">
